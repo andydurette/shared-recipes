055c58ae591ae7436f8b7a828705d227e8c7bc31
Andy Durette <duretteandy@gmail.com>
2021-06-12 20:53:26 -0400

    Add file setup

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
+    app.use(express.static(path.join(__dirname, "../", "client", "build")));
 }
 
 //Connect to Routes
